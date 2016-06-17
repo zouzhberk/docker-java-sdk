895f6f0328768a1a22b24b441538ed8a54fc03ad
berk <zouzhberk@163.com>
2016-06-17 18:10:22 +0800

    update

---
+++ src/main/java/com/github/zouzhberk/docker/client/DockerClient.java
@@ -0,0 +1,14 @@
+package com.github.zouzhberk.docker.client;
+
+/**
+ * Created by berk (zouzhberk@163.com)) on 6/17/16.
+ */
+public class DockerClient
+{
+
+    public static class Builder
+    {
+
+    }
+
+}
---
+++ src/main/java/com/github/zouzhberk/docker/client/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by berk (zouzhberk@163.com)) on 6/17/16.
+ */
+package com.github.zouzhberk.docker.client.domain;
